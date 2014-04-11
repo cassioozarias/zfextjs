8da9930466f471f1c4e3dfdf70e8bfba3b82eef7
cassio <cassio.ozarias@gmail.com>
2014-04-11 16:27:51 -0400

    [ADJUST] Criado os controllers
    Signed-off-by: cassio <cassio.ozarias@gmail.com>

--- module/Loja/src/Loja/Entity/InstrumentoRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Loja\Entity;
-
-use Doctrine\ORM\EntityRepository;
-
-class InstrumentoRepository extends EntityRepository {
-    
-}
--- module/Loja/src/Loja/Entity/MusicoRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Loja\Entity;
-
-use Doctrine\ORM\EntityRepository;
-
-class MusicoRepository extends EntityRepository {
-    
-}
